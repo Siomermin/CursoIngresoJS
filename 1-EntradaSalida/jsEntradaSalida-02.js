87a98a95efc9ea697189f43f34b841bcc972ea8a
Siomermin <fermingonzalez011@gmail.com>
2021-07-06 12:01:56 -0300

    Ejercicios 06/07/21

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombreUsuario; // Declaracion de una variable con la palabra reservada let.
+
+	nombreUsuario = prompt("Cual es tu nombre?");
+
+	alert(nombreUsuario);
+
+
 }
 
